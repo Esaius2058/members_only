77596a2d21d6cb59baff0cfdbfacc37de754a38e
Esaius2058 <wabwirewild@gmail.com>
2025-01-29 20:11:02 +0300

    Fix response structure for fetching user posts
    Ensure correct type conversion for request parameters
    
    Standardize API responses for post-related operations

---
+++ src/controllers/messageController.ts
@@ -0,0 +1,50 @@
+import { Response, Request, NextFunction } from "express";
+import { handleCreatePost, handleDeletePost, handleGetPostsByUserId } from "../db/messageQueries";
+
+interface MessageOutput{
+	message: string;
+	userId: number;
+	messageId?: number;
+	createdAt?: Date;
+}
+
+export async function createPost(req: Request, res: Response): Promise<void>{
+	const {message, userId} = req.body;
+
+	try{
+		const post = await handleCreatePost({message, userId});
+		res.status(201).json({message: post.message, userId: post.userId, createdAt: post.createdAt});
+	}catch(error: unknown){
+		console.error("Internal server error", error);
+    res.status(500).json({message: "Internal server error"});
+	}
+}
+
+export async function getPostsByUserId(req: Request, res: Response) {
+	const {userId} = req.params;
+
+	try{
+		const userPosts = await handleGetPostsByUserId(Number(userId));
+
+		if (!userPosts.length) {
+      return res.status(404).json({ message: "No messages found for this user" });
+    }
+
+		res.status(200).json({message: "User's posts retreived successfully", userId, posts: userPosts});
+	}catch(error: unknown){
+		console.error("Internal server error", error);
+    res.status(500).json({message: "Internal server error"});
+	}
+}
+
+export async function deletePost(req: Request, res: Response): Promise<void>{
+	const {messageId} = req.params;
+
+	try{
+		await handleDeletePost(Number(messageId));
+		res.status(200).json({message: "Successfully deleted message", messageId: messageId});
+	}catch(error: unknown){
+		console.error("Internal server error", error);
+    res.status(500).json({message: "Internal server error"});
+	}
+}
