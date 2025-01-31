12f6d3a018889309322baaed17c51e096088cdff
Esaius2058 <wabwirewild@gmail.com>
2025-01-31 11:25:34 +0300

    Update the getPostsByUserId function to check the membership status first before retreiving the messages
    Added passcode to the User interface

--- src/db/messageQueries.ts
+++ src/db/messageQueries.ts
@@ -1,74 +1,90 @@
 import pool from "./pool";
 
-interface MessageOutput{
-	message: string;
-	userId: number;
-	messageId?: number;
-	createdAt?: Date;
-	posts?: MessageOutput[];
+interface MessageOutput {
+  message: string;
+  userId: number;
+  messageId?: number;
+  createdAt?: Date;
+  posts?: MessageOutput[];
 }
 
 interface MessageResponse {
-	messageId?: number;
-	status: number;
-	message: string;
+  messageId?: number;
+  status: number;
+  message: string;
 }
 
-export async function handleCreatePost({message, userId}:MessageOutput):Promise<MessageOutput> {
-	if(!message || !userId){
-		throw new Error("Invaid input: Message content and user ID must be provided");
-	}
+export async function handleCreatePost({
+  message,
+  userId,
+}: MessageOutput): Promise<MessageOutput> {
+  if (!message || !userId) {
+    throw new Error(
+      "Invaid input: Message content and user ID must be provided"
+    );
+  }
 
-	const query = `insert into messages (text, user_id) values ($1, $2) returning message_id as messageId, content as message, created_at as createdAt;`;
-	const values = [message, userId]
-	try{
-		const result = await pool.query(query, values);
+  const query = `insert into messages (text, user_id) values ($1, $2) returning message_id as messageId, content as message, created_at as createdAt;`;
+  const values = [message, userId];
+  try {
+    const result = await pool.query(query, values);
 
-		if(result.rows.length === 0){
-			throw new Error("Failed to create message");
-		}
+    if (result.rows.length === 0) {
+      throw new Error("Failed to create message");
+    }
 
-		return result.rows[0];
-	}catch(error: unknown){
-		console.error("Error posting message");
-		throw new Error("Error posting message");
-	}
+    return result.rows[0];
+  } catch (error: unknown) {
+    console.error("Error posting message");
+    throw new Error("Error posting message");
+  }
 }
 
-export async function handleDeletePost(id: number):Promise<MessageResponse>{
-	const query =  `delete from messages where message_id = $1 returning message_id`;
-	const values = [id];
-	try{
-		const result =  await pool.query(query, values);
+export async function handleDeletePost(id: number): Promise<MessageResponse> {
+  const query = `delete from messages where message_id = $1 returning message_id`;
+  const values = [id];
+  try {
+    const result = await pool.query(query, values);
 
-		if (result.rowCount === 0) {
+    if (result.rowCount === 0) {
       return {
         message: "Post not found",
         status: 404,
       };
     }
-		return{
-			message: "Post deleted successfully",
-			status: 200,
-			messageId: result.rows[0].messageid,
-		}
-	}catch(error: unknown){
-		console.error("Error deleting post", error);
-		throw new Error("Error deleting post");
-	}
+    return {
+      message: "Post deleted successfully",
+      status: 200,
+      messageId: result.rows[0].messageid,
+    };
+  } catch (error: unknown) {
+    console.error("Error deleting post", error);
+    throw new Error("Error deleting post");
+  }
 }
 
-export async function handleGetPostsByUserId(id: number): Promise<MessageOutput[]>{
-	try{
-		const result = await pool.query("select text as message, created_at as createdAt from messages where user_id = $1", [id]);
-		
-		if(result.rowCount === 0){
-			throw new Error("No posts found for the user");
-		}
+export async function handleGetPostsByUserId(
+  id: number
+): Promise<MessageOutput[]> {
+  try {
+    const user = await pool.query(
+      "select membership from users where user_id = $1",
+      [id]
+    );
+    const isMember = user.rows[0]?.membership;
 
-		return result.rows as MessageOutput[];
-	}catch(error: unknown){
-		console.error("Error fetching posts",error);
-		throw new Error("Error fetching posts");
-	}
+    const query = isMember
+      ? "select messages.messageid, messages.text, users.username from messages join users on messages.user_id = users.user_id"
+      : "select message_id, text from messages";
+    const result = await pool.query(query);
+
+    if (result.rowCount === 0) {
+      throw new Error("No posts found for the user");
+    }
+
+    return result.rows as MessageOutput[];
+  } catch (error: unknown) {
+    console.error("Error fetching posts", error);
+    throw new Error("Error fetching posts");
+  }
 }
