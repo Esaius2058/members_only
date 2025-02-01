1aec33b3a9475a75c3ac11abed7024c5c8588c00
Esaius2058 <wabwirewild@gmail.com>
2025-02-01 14:39:30 +0300

    Set up views directory path in express configuration
    Updated view engine configuration to use correct views directory path
    
    Debugged views directory path: verified correct path set for ejs view
    
    Removed the debugging log

---
+++ src/app.ts
@@ -0,0 +1,24 @@
+import * as dotenv from "dotenv";
+import express, {Application, json} from "express";
+import session from "express-session";
+import passport from "passport";
+import moRouter from "./routers/moRouters";
+import path from "path";
+dotenv.config();
+
+const app: Application = express();
+
+app.set("view engine", "ejs");
+app.set("views", path.resolve(__dirname, "../src","views"));
+
+// Middleware to parse incoming JSON requests into JavaScript objects
+app.use(express.json());
+// Middleware to parse URL-encoded data (like form submissions)
+app.use(express.urlencoded({extended: true}));
+app.use(session({secret: "cats", resave: false, saveUninitialized: false}));
+app.use(passport.session());
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/", moRouter);
+
+const PORT = process.env.PORT ? parseInt(process.env.PORT) : 3000;
+app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
