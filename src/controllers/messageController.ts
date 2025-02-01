12e95cbe8f5fa2e1a91fa121f99af6cb980fe599
Esaius2058 <wabwirewild@gmail.com>
2025-02-01 08:48:32 +0300

    format code to enhance readability

--- src/controllers/messageController.ts
+++ src/controllers/messageController.ts
@@ -1,50 +1,71 @@
 import { Response, Request, NextFunction } from "express";
-import { handleCreatePost, handleDeletePost, handleGetPostsByUserId } from "../db/messageQueries";
+import {
+  handleCreatePost,
+  handleDeletePost,
+  handleGetPostsByUserId,
+} from "../db/messageQueries";
 
-interface MessageOutput{
-	message: string;
-	userId: number;
-	messageId?: number;
-	createdAt?: Date;
+interface MessageOutput {
+  message: string;
+  userId: number;
+  messageId?: number;
+  createdAt?: Date;
 }
 
-export async function createPost(req: Request, res: Response): Promise<void>{
-	const {message, userId} = req.body;
+export async function createPost(req: Request, res: Response): Promise<void> {
+  const { message, userId } = req.body;
 
-	try{
-		const post = await handleCreatePost({message, userId});
-		res.status(201).json({message: post.message, userId: post.userId, createdAt: post.createdAt});
-	}catch(error: unknown){
-		console.error("Internal server error", error);
-    res.status(500).json({message: "Internal server error"});
-	}
+  try {
+    const post = await handleCreatePost({ message, userId });
+    res
+      .status(201)
+      .json({
+        message: post.message,
+        userId: post.userId,
+        createdAt: post.createdAt,
+      });
+  } catch (error: unknown) {
+    console.error("Internal server error", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
 }
 
-export async function getPostsByUserId(req: Request, res: Response):Promise<void> {
-	const {userId} = req.params;
+export async function getPostsByUserId(
+  req: Request,
+  res: Response
+): Promise<void> {
+  const { userId } = req.params;
 
-	try{
-		const userPosts = await handleGetPostsByUserId(Number(userId));
+  try {
+    const userPosts = await handleGetPostsByUserId(Number(userId));
 
-		if (!userPosts.length) {
-      	res.status(404).json({ message: "No messages found for this user" });
+    if (!userPosts.length) {
+      res.status(404).json({ message: "No messages found for this user" });
     }
 
-		res.status(200).json({message: "User's posts retreived successfully", userId, posts: userPosts});
-	}catch(error: unknown){
-		console.error("Internal server error", error);
-    res.status(500).json({message: "Internal server error"});
-	}
+    res
+      .status(200)
+      .json({
+        message: "User's posts retreived successfully",
+        userId,
+        posts: userPosts,
+      });
+  } catch (error: unknown) {
+    console.error("Internal server error", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
 }
 
-export async function deletePost(req: Request, res: Response): Promise<void>{
-	const {messageId} = req.params;
+export async function deletePost(req: Request, res: Response): Promise<void> {
+  const { messageId } = req.params;
 
-	try{
-		await handleDeletePost(Number(messageId));
-		res.status(200).json({message: "Successfully deleted message", messageId: messageId});
-	}catch(error: unknown){
-		console.error("Internal server error", error);
-    res.status(500).json({message: "Internal server error"});
-	}
+  try {
+    await handleDeletePost(Number(messageId));
+    res
+      .status(200)
+      .json({ message: "Successfully deleted message", messageId: messageId });
+  } catch (error: unknown) {
+    console.error("Internal server error", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
 }
