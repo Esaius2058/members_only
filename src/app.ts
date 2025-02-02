166a838a6c31df9514b5362dc40d626626f30702
Esaius2058 <wabwirewild@gmail.com>
2025-02-02 20:08:53 +0300

    set the app to use Express.user when referring to locals.user

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import * as dotenv from "dotenv";
-import express, {Application, json} from "express";
+import express, { Application } from "express";
 import session from "express-session";
 import passport from "passport";
 import moRouter from "./routers/moRouters";
@@ -9,16 +9,20 @@ dotenv.config();
 const app: Application = express();
 
 app.set("view engine", "ejs");
-app.set("views", path.resolve(__dirname, "../src","views"));
+app.set("views", path.resolve(__dirname, "../src", "views"));
 
 // Middleware to parse incoming JSON requests into JavaScript objects
 app.use(express.json());
 // Middleware to parse URL-encoded data (like form submissions)
-app.use(express.urlencoded({extended: true}));
-app.use(session({secret: "cats", resave: false, saveUninitialized: false}));
+app.use(express.urlencoded({ extended: true }));
+app.use(session({ secret: "cats", resave: false, saveUninitialized: false }));
 app.use(passport.session());
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/", moRouter);
+app.use((req, res, next) => {
+  res.locals.user = req.user;
+  next();
+});
 
 const PORT = process.env.PORT ? parseInt(process.env.PORT) : 3000;
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
