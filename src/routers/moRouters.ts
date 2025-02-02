d177b829fde764070cbb827e3ba3546ace2b0010
Esaius2058 <wabwirewild@gmail.com>
2025-02-02 20:05:15 +0300

    create routers for logout, dashboard and user-profile

--- src/routers/moRouters.ts
+++ src/routers/moRouters.ts
@@ -4,11 +4,13 @@ import {
   getUserById,
   updateUser,
   loginUser,
+  logoutUser,
 } from "../controllers/userController";
 import {
   createPost,
   getPostsByUserId,
   deletePost,
+  loadUserProfile
 } from "../controllers/messageController";
 
 const moRouter = Router();
@@ -25,11 +27,19 @@ moRouter.get("/users/log-in", (req, res) => {
     res.render("log-in");
 });
 moRouter.post("/users/log-in", loginUser);
+moRouter.post("/users/log-out", logoutUser);
+moRouter.get("/dashboard", loadUserProfile);
+moRouter.get("/user-profile", (req, res) => {
+  res.render("user-profile", {title: "User Profile", user: req.user});
+});
 moRouter.get("/users/:userId", getUserById);
 moRouter.put("/users/update/:userId", updateUser);
 
 //Messages Routes
-moRouter.post("/messages/post", createPost);
+moRouter.get("/posts/new", (req, res) => {
+  res.render("new_post", {title: "New Post"});
+});
+moRouter.post("/posts/new", createPost);
 moRouter.get("/messages/:userId", getPostsByUserId);
 moRouter.delete("/messages/delete/:messageId", deletePost);
 
