4f07fa1b96f5a9b93d7d917700759083a087d3af
Esaius2058 <wabwirewild@gmail.com>
2025-02-06 23:09:23 +0300

    make userId and userName optional in the UserResponse interface
    add id in the parameters for updateUser
    
    return the fullname and membership in handleUpdateUser function
    
    add a condition for when a premium member tries to become a premium member again in handleAddMember
    
    create function handleAdminAuthentication
    
    obtain the admin passcode from the environment

--- src/db/userQueries.ts
+++ src/db/userQueries.ts
@@ -1,13 +1,14 @@
 import pool from "./pool";
 import bcrypt from "bcryptjs";
-import passport, { use } from "passport";
+import passport from "passport";
 import { Strategy as LocalStrategy } from "passport-local";
+import * as dotenv from "dotenv";
 
 export interface CustomUser {
   id?: number;
   fullname?: string;
   username: string;
-  password: string;
+  password?: string;
   membership?: boolean;
   passcode?: string;
 }
@@ -15,9 +16,10 @@ export interface CustomUser {
 interface UserResponse {
   message: string;
   status: number;
-  userId: number;
-  userName: string;
+  userId?: number;
+  userName?: string;
   fullName?: string;
+  membership?: boolean;
 }
 
 export async function handleCreateUser({
@@ -93,17 +95,19 @@ passport.deserializeUser(async (id: number, done) => {
 });
 
 export async function handleUpdateUser({
+  id,
   fullname,
   username,
-  password,
+  password
 }: CustomUser): Promise<UserResponse> {
+
   if (!fullname || !username || !password) {
     throw new Error("Invalid input: All fields must be provided");
   }
 
   const hashedPassword = await bcrypt.hash(password, 10);
-  const query = `update users set fullname = $1, username = $2, password = $3 where username = $2 returning user_id`;
-  const values = [fullname, username, hashedPassword];
+  const query = `update users set fullname = $1, username = $2 where user_id= $3returning *`;
+  const values = [fullname, username, id];
 
   try {
     const result = await pool.query(query, values);
@@ -112,10 +116,11 @@ export async function handleUpdateUser({
       status: 200,
       userId: result.rows[0].user_id,
       userName: result.rows[0].username,
-      fullName: result.rows[0].fullname
+      fullName: result.rows[0].fullname,
+      membership: result.rows[0].membership
     };
   } catch (error) {
-    console.error("Error updating user");
+    console.error("Error updating user", error);
     throw new Error("Error updating user");
   }
 }
@@ -139,9 +144,11 @@ export async function handleGetUserById(id: number): Promise<CustomUser> {
 
 export async function handleAddMember(id: number): Promise<UserResponse>{ 
   try{
-    const result = await pool.query("update users set membership = true where user_id = $1 returning user_id, username", [id]);
+    const result = await pool.query("update users set membership = true where user_id = $1 returning user_id, username, membership", [id]);
     
     if (result.rowCount === 0) {
+      throw new Error("User not found or already a member");
+    } else if(result.rows[0].membership){
       throw new Error("User not found or already a member");
     }
 
@@ -155,7 +162,31 @@ export async function handleAddMember(id: number): Promise<UserResponse>{
     console.error("Error updating membership", error);
     throw new Error("Error updating membership");
   }
+}
+
+export async function handleAdminAuthentication(id: number, passcode: string): Promise<UserResponse> {
+  const adminPasscode = process.env.ADMINPASSCODE;
+  if(passcode !== adminPasscode){
+    return { 
+      message: "Wrong Passcode!!",
+      status: 401, 
+    }
+  }
   
+  const result = await pool.query("update users set admin = true where user_id = $1 returning user_id, username, admin", [id]);
+
+  if (result.rowCount === 0) {
+    throw new Error("User not found");
+  }else if(result.rows[0].admin){
+    throw new Error("User is an admin");
+  }
+
+  return {
+    message: "You're now have admin priviledges!!",
+    status: 200,
+    userId: result.rows[0].id,
+    userName: result.rows[0].username
+  };
 }
 
 export default passport;
